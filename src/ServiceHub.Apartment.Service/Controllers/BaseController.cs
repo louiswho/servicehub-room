37f39f6c179cc715140f473bd6768d56d8f2d1dd
fred belotte <fred@belotte.xyz>
2018-05-14 12:32:15 -0400

    update the base controller. [#157544928]

--- src/ServiceHub.Apartment.Service/Controllers/BaseController.cs
+++ src/ServiceHub.Apartment.Service/Controllers/BaseController.cs
@@ -1,6 +1,4 @@
-using System;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Logging;
 
 namespace ServiceHub.Apartment.Service.Controllers
@@ -8,14 +6,10 @@ namespace ServiceHub.Apartment.Service.Controllers
   public abstract class BaseController : Controller
   {
     protected readonly ILogger logger;
-    protected readonly IQueueClient queueClient;
-    protected readonly string connectionString = Environment.GetEnvironmentVariable("SERVICE_BUS_CONNECTION_STRING");
-    protected readonly string queueName = Environment.GetEnvironmentVariable("SERVICE_BUS_QUEUE_NAME");
 
     protected BaseController(ILoggerFactory loggerFactory)
     {
       logger = loggerFactory.CreateLogger(this.GetType().Name);
-      queueClient = new QueueClient(connectionString, queueName);
     }
   }
 }
