2ee412548dbc3555c14e8ec2020dce2b46c6a9c8
fred belotte <fred@belotte.xyz>
2018-05-14 11:56:12 -0400

    add the service bus queue client to the base controller. [#157544928]

--- src/ServiceHub.Apartment.Service/Controllers/BaseController.cs
+++ src/ServiceHub.Apartment.Service/Controllers/BaseController.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Logging;
 
 namespace ServiceHub.Apartment.Service.Controllers
@@ -6,10 +8,14 @@ namespace ServiceHub.Apartment.Service.Controllers
   public abstract class BaseController : Controller
   {
     protected readonly ILogger logger;
+    protected readonly IQueueClient queueClient;
+    protected readonly string connectionString = Environment.GetEnvironmentVariable("SERVICE_BUS_CONNECTION_STRING");
+    protected readonly string queueName = Environment.GetEnvironmentVariable("SERVICE_BUS_QUEUE_NAME");
 
     protected BaseController(ILoggerFactory loggerFactory)
     {
       logger = loggerFactory.CreateLogger(this.GetType().Name);
+      queueClient = new QueueClient(connectionString, queueName);
     }
   }
 }
