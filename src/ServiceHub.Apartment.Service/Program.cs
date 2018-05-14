8b3e490a51b2ae9332bf7375e20f550f38e657df
fred belotte <fred@belotte.xyz>
2018-05-14 12:35:24 -0400

    add the queue implementation. [#157544928]

--- src/ServiceHub.Apartment.Service/Program.cs
+++ src/ServiceHub.Apartment.Service/Program.cs
@@ -5,16 +5,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using ServiceHub.Apartment.Service.Controllers;
 
 namespace ServiceHub.Apartment.Service
 {
   public static class Program
   {
+    private static readonly ILoggerFactory _loggerFactory = new LoggerFactory();
+    private static readonly IQueueClient _queueClient = new QueueClient(_connectionString, _queueName);
+    private static readonly string _connectionString = Environment.GetEnvironmentVariable("SERVICE_BUS_CONNECTION_STRING");
+    private static readonly string _queueName = Environment.GetEnvironmentVariable("SERVICE_BUS_QUEUE_NAME");
+    
     public static void Main(string[] args)
     {
       BuildWebHost(args).Run();
+      QueueAsync().GetAwaiter().GetResult();
     }
 
     public static IWebHost BuildWebHost(string[] args) =>
@@ -22,5 +30,11 @@ namespace ServiceHub.Apartment.Service
         .UseApplicationInsights(Environment.GetEnvironmentVariable("INSTRUMENTATION_KEY"))
         .UseStartup<Startup>()
         .Build();
+    
+    private static async Task QueueAsync()
+    {
+      var queue = new QueueController(_loggerFactory);
+      await Task.Run(() => {});
+    }
   }
 }
