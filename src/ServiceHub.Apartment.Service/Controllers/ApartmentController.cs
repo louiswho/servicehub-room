b7f8d5cf970b566f4a4e7a4490988e706b80670a
fred belotte <fred@belotte.xyz>
2018-05-15 12:13:48 -0400

    refactor the messaging implementation. [#157544928]

--- src/ServiceHub.Apartment.Service/Controllers/ApartmentController.cs
+++ src/ServiceHub.Apartment.Service/Controllers/ApartmentController.cs
@@ -1,14 +1,17 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Logging;
+using ServiceHub.Apartment.Library.Interfaces;
 
 namespace ServiceHub.Apartment.Service.Controllers
 {
   [Route("api/[controller]")]
   public class ApartmentController : BaseController
   {
-    public ApartmentController(ILoggerFactory loggerFactory) : base(loggerFactory) {}
-    
+    public ApartmentController(ILoggerFactory loggerFactory, IQueueClient queueClientSingleton) : base(loggerFactory, queueClientSingleton) {}
+
     public async Task<IActionResult> Get()
     {
       return await Task.Run(() => Ok());
@@ -36,6 +39,15 @@ namespace ServiceHub.Apartment.Service.Controllers
     public async Task<IActionResult> Delete(int id)
     {
       return await Task.Run(() => Ok());
+    }
+
+    protected override void UseReceiver()
+    {
+      queueClient.RegisterMessageHandler(ReceiverMessageProcessAsync, ReceiverExceptionHandler);
+    }
+    protected override void UseSender()
+    {
+      queueClient.SendAsync(new Message());
     }
   }
 }
--- src/ServiceHub.Apartment.Service/Controllers/BaseController.cs
+++ src/ServiceHub.Apartment.Service/Controllers/BaseController.cs
@@ -1,15 +1,43 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Logging;
+using ServiceHub.Apartment.Library.Factories;
+using ServiceHub.Apartment.Library.Interfaces;
 
 namespace ServiceHub.Apartment.Service.Controllers
 {
   public abstract class BaseController : Controller
   {
     protected readonly ILogger logger;
+    protected readonly IQueueClient queueClient;
+    protected abstract void UseReceiver();
+    protected abstract void UseSender();
 
-    protected BaseController(ILoggerFactory loggerFactory)
+    protected BaseController(ILoggerFactory loggerFactory, IQueueClient queueClientSingleton)
     {
       logger = loggerFactory.CreateLogger(this.GetType().Name);
+      queueClient = queueClientSingleton;
+    }
+
+    protected virtual async Task ReceiverMessageProcessAsync(Message message, CancellationToken cancellationToken)
+    {
+      logger.LogInformation(message.MessageId);
+      await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+    }
+
+    protected virtual async Task SenderMessageProcessAsync(Message message)
+    {
+      logger.LogInformation(message.MessageId);
+      await queueClient.SendAsync(message);
+    }
+
+    protected virtual async Task ReceiverExceptionHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+    {
+      logger.LogError(exceptionReceivedEventArgs.Exception.ToString());
+      await Task.CompletedTask;
     }
   }
 }
