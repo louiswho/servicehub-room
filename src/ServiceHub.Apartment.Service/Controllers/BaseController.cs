ec9406880410d3ea18a5b4d8c36f98359fd1ba4f
fred belotte <fred@belotte.xyz>
2018-05-15 00:54:13 -0400

    update the base controller. [#157544928]

--- src/ServiceHub.Apartment.Service/Controllers/BaseController.cs
+++ src/ServiceHub.Apartment.Service/Controllers/BaseController.cs
@@ -6,8 +6,9 @@ namespace ServiceHub.Apartment.Service.Controllers
   public abstract class BaseController : Controller
   {
     protected readonly ILogger logger;
+    private static readonly ILoggerFactory loggerFactory = new LoggerFactory();
 
-    protected BaseController(ILoggerFactory loggerFactory)
+    protected BaseController()
     {
       logger = loggerFactory.CreateLogger(this.GetType().Name);
     }
