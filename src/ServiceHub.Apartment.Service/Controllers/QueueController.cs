3767f7d9ef2d4183a6ddcc3436e774084bb38550
fred belotte <fred@belotte.xyz>
2018-05-14 21:17:05 -0400

    add the messaging queue scaffolding. [#157544928]

--- src/ServiceHub.Apartment.Service/Controllers/QueueController.cs
+++ src/ServiceHub.Apartment.Service/Controllers/QueueController.cs
@@ -1,9 +1,39 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Logging;
 
 namespace ServiceHub.Apartment.Service.Controllers
 {
   public class QueueController : BaseController
   {
+    private static readonly IQueueClient _queueClient = new QueueClient(_connectionString, _queueName);
+    private static readonly string _connectionString = Environment.GetEnvironmentVariable("SERVICE_BUS_CONNECTION_STRING");
+    private static readonly string _queueName = Environment.GetEnvironmentVariable("SERVICE_BUS_QUEUE_NAME");
+
     public QueueController(ILoggerFactory loggerFactory) : base(loggerFactory) {}
+
+    public static void UseMessagingQueue()
+    {
+      var queue = new QueueController(new LoggerFactory());
+      var messageHandlerOptions = new MessageHandlerOptions(queue.ExceptionHandler)
+      {
+        AutoComplete = false,
+        MaxConcurrentCalls = 1
+      };
+
+      _queueClient.RegisterMessageHandler(queue.MessageHandler, messageHandlerOptions);
+    }
+
+    private async Task ExceptionHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+    {
+      await Task.Run(() => throw new NotImplementedException());
+    }
+
+    private async Task MessageHandler(Message message, CancellationToken cancellationToken)
+    {
+      await Task.Run(() => throw new NotImplementedException());
+    }
   }
 }
--- src/ServiceHub.Apartment.Service/Program.cs
+++ src/ServiceHub.Apartment.Service/Program.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using ServiceHub.Apartment.Service.Controllers;
@@ -14,15 +13,10 @@ namespace ServiceHub.Apartment.Service
 {
   public static class Program
   {
-    private static readonly ILoggerFactory _loggerFactory = new LoggerFactory();
-    private static readonly IQueueClient _queueClient = new QueueClient(_connectionString, _queueName);
-    private static readonly string _connectionString = Environment.GetEnvironmentVariable("SERVICE_BUS_CONNECTION_STRING");
-    private static readonly string _queueName = Environment.GetEnvironmentVariable("SERVICE_BUS_QUEUE_NAME");
-    
     public static void Main(string[] args)
     {
       BuildWebHost(args).Run();
-      QueueAsync().GetAwaiter().GetResult();
+      QueueController.UseMessagingQueue();
     }
 
     public static IWebHost BuildWebHost(string[] args) =>
@@ -30,11 +24,5 @@ namespace ServiceHub.Apartment.Service
         .UseApplicationInsights(Environment.GetEnvironmentVariable("INSTRUMENTATION_KEY"))
         .UseStartup<Startup>()
         .Build();
-    
-    private static async Task QueueAsync()
-    {
-      var queue = new QueueController(_loggerFactory);
-      await Task.Run(() => {});
-    }
   }
 }
