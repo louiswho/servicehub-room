591a65e7ecce156dbea16228492c5c945e8699fb
Cameron Wagstaff <cameron.wagstaff.129@gmail.com>
2018-06-12 15:14:16 -0400

    Added Room models. [#157252834]
    Context and library models are separated, with library models having validation and context models having annotations for MongoDb

---
+++ src/ServiceHub.Room.Library/Models/Address.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace ServiceHub.Room.Library.Models
+{
+    ///<summary> Address Model Referenced by the Room model. </summary>
+    ///<remarks>This model is for the library and has validation.</remarks>
+    [DataContract]
+    public class Address {
+
+        /// <summary>
+        /// List of all valid state codes.
+        /// </summary>
+        /// <remarks>
+        /// Values are uppercase for case insensitivity.
+        /// </remarks>
+        [IgnoreDataMember]
+        private static readonly string[] StateCodes = {
+            "AL", "AK", "AZ", "AR", "CA",
+            "CO", "CT", "DE", "FL", "GA",
+            "HI", "ID", "IL", "IN", "IA",
+            "KS", "KY", "LA", "ME", "MD",
+            "MA", "MI", "MN", "MS", "MO",
+            "MT", "NE", "NV", "NH", "NJ",
+            "NM", "NY", "NC", "ND", "OH",
+            "OK", "OR", "PA", "RI", "SC",
+            "SD", "TN", "TX", "UT", "VT",
+            "VA", "WA", "WV", "WI", "WY"
+        };
+
+        /// <summary>
+        /// List of all valid country codes.
+        /// </summary>
+        /// <remarks>
+        /// Values are uppercase for case insensitivity.
+        /// </remarks>
+        [IgnoreDataMember]
+        private static readonly string[] CountryCodes = { "US" };
+
+        ///<summary> Key. Used to uniquely identify this Address model. </summary>
+        [DataMember]
+        public Guid AddressId { get; set; }
+
+        ///<summary> Steet number and street name of this address. </summary>
+        [DataMember]
+        public string Address1 { get; set; }
+
+        ///<summary> Apt/Room number for this address if applicable. </summary>
+        [DataMember]
+        public string Address2 { get; set; }
+
+        ///<summary> Name of the city for this address. </summary>
+        [DataMember]
+        public string City { get; set; }
+
+        ///<summary> Name of the state for this address. </summary>
+        [DataMember]
+        public string State { get; set; }
+
+        ///<summary> 5 digit number specifing region. </summary>
+        [DataMember]
+        public string PostalCode { get; set; }
+
+        ///<summary> Name of the country for this address. </summary>
+        [DataMember]
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Initialize an Address with invalid properties.
+        /// </summary>
+        public Address() {
+            AddressId = Guid.Empty;
+            Address1 = null;
+            Address2 = null;
+            City = null;
+            State = null;
+            PostalCode = null;
+            Country = null;
+        }
+
+        /// <summary>
+        /// Checks if state of model is valid
+        /// </summary>
+        /// <returns>True if the model is valid, false otherwise.</returns>
+        public bool isValidState()
+        {
+            if (AddressId == Guid.Empty) { return false; }
+            if (String.IsNullOrEmpty(Address1) || Address1?.Length > 255) { return false; }
+            if (Address2?.Length > 255) { return false; }
+            if (String.IsNullOrEmpty(City) || City?.Length > 25) { return false; }
+            if (String.IsNullOrEmpty(State) || !StateCodes.Contains(State.ToUpper())) { return false; }
+            if (String.IsNullOrEmpty(PostalCode) || PostalCode.Length != 5 || !PostalCode.All(Char.IsDigit)) { return false; }
+            if (String.IsNullOrEmpty(Country) || !CountryCodes.Contains(Country.ToUpper())) { return false; }
+
+            return true;
+        }
+    }
+}
---
+++ src/ServiceHub.Room.Testing/Library/AddressTest.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using ServiceHub.Room.Library.Models;
+using Xunit;
+
+namespace ServiceHub.Room.Testing.Library
+{
+    public class AddressTest
+    {
+        [Fact]
+        public void TestAddressValidationAll()
+        {
+            Address address = new Address();
+            
+            Assert.False(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationAddressId()
+        {
+            Address address = new Address
+            {
+                Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                City = "Tampa",
+                Country = "US",
+                PostalCode = "92646",
+                State = "Ca"
+            };
+
+            Assert.False(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationAddress1()
+        {
+            Address address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                //Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                City = "Tampa",
+                Country = "US",
+                PostalCode = "92646",
+                State = "Ca"
+            };
+            
+            Assert.False(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationAddress2()
+        {
+            Address address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                Address1 = "1234 Test st.",
+                //Address2 = "apt 303",
+                City = "Tampa",
+                Country = "US",
+                PostalCode = "92646",
+                State = "Ca"
+            };
+            
+            Assert.True(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationCity()
+        {
+            Address address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                //City = "Tampa",
+                Country = "US",
+                PostalCode = "92646",
+                State = "Ca"
+            };
+            
+            Assert.False(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationState()
+        {
+            Address address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                City = "Tampa",
+                Country = "US",
+                PostalCode = "92646",
+                //State = "Ca"
+            };
+            
+            Assert.False(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationStateToLong()
+        {
+            Address address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                City = "Tampa",
+                Country = "US",
+                PostalCode = "92646",
+                State = "Cal"
+            };
+            
+            Assert.False(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationStateToShort()
+        {
+            Address address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                City = "Tampa",
+                Country = "US",
+                PostalCode = "92646",
+                State = "C"
+            };
+
+            Assert.False(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationPostalCode()
+        {
+            Address address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                City = "Tampa",
+                Country = "US",
+                //PostalCode = "92646",
+                State = "Ca"
+            };
+            
+            Assert.False(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationPostalCodeToLong()
+        {
+            Address address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                City = "Tampa",
+                Country = "US",
+                PostalCode = "926464",
+                State = "Ca"
+            };
+            
+            Assert.False(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationPostalCodeToShort()
+        {
+            Address address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                City = "Tampa",
+                Country = "US",
+                PostalCode = "9264",
+                State = "Ca"
+            };
+            
+            Assert.False(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationCountry()
+        {
+            Address address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                City = "Tampa",
+                //Country = "US",
+                PostalCode = "92646",
+                State = "Ca"
+            };
+            
+            Assert.False(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationCountryToLong()
+        {
+            Address address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                City = "Tampa",
+                Country = "USa",
+                PostalCode = "92646",
+                State = "Ca"
+            };
+            
+            Assert.False(address.isValidState());
+        }
+
+        [Fact]
+        public void TestAddressValidationCountryToShort()
+        {
+            Address address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                City = "Tampa",
+                Country = "U",
+                PostalCode = "92646",
+                State = "Ca"
+            };
+            
+            Assert.False(address.isValidState());
+        }
+    }
+}
