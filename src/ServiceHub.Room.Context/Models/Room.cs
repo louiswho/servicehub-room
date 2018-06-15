4cd8c673bf2fc00e4c2f6b237d13e7959dd018c3
Cameron Wagstaff <cameron.wagstaff.129@gmail.com>
2018-06-15 13:28:58 -0400

    Bringing in context work. [#157252835]

---
+++ src/ServiceHub.Room.Context/Models/Room.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ServiceHub.Room.Context.Models
+{
+    /// <summary>
+    /// Room model.
+    /// </summary>
+    /// <remarks>
+    /// This model is for the context and is assumed to be valid.
+    /// </remarks>
+    public class Room
+    {
+        ///<summary> Key. Used to uniquely identify this Room model. </summary>
+        [BsonId]
+        public Guid RoomId { get; set; }
+
+        ///<summary> Name of the training location where this room is
+        ///located.  Example: "Reston", "Tampa" or, "Dallas".
+        ///</summary>
+        public string Location { get; set; }
+
+        ///<summary> The Address model for this room. </summary>
+        public Address Address { get; set; }
+
+        ///<summary> Number of unassigned beds in this room. </summary>
+        public int Vacancy { get; set; }
+
+        ///<summary> Total number of beds in this room weather assigned or not</summary>
+        public int Occupancy { get; set; }
+
+        ///<summary> Sex that this room accommodates. </summary>
+        public string Gender { get; set; }
+    }
+}
---
+++ src/ServiceHub.Room.Context/Repository/RoomContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceHub.Room.Context.Repository
+{
+    public class RoomContext
+    {
+        private readonly IRoomsRepository _roomRepository;
+
+        /// <summary>
+        /// Instantiates a IRoomsRepository object for CRUD functionality on a data source.
+        /// </summary>
+        /// <param name="roomRepository"></param>
+        public RoomContext(IRoomsRepository roomRepository)
+        {
+            _roomRepository = roomRepository;
+        }
+
+        /// <summary>
+        /// Creates a Models.Room object in the data source.
+        /// </summary>
+        /// <param name="room"></param>
+        public void Insert(Models.Room room)
+        {
+            _roomRepository.Insert(room);
+        }
+
+        /// <summary>
+        /// Gets all Models.Room objects from a data source.
+        /// </summary>
+        /// <returns>Returns all Models.Room objects from the give data source.</returns>
+        public List<Models.Room> Get()
+        {
+            return _roomRepository.Get();
+        }
+
+        /// <summary>
+        /// Gets a Models.Room objects from a data source.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>>Returns Models.Room objects from the give data source based on the Guid.</returns>
+        public Models.Room GetById(Guid id)
+        {
+            return _roomRepository.GetById(id);
+        }
+
+        /// <summary>
+        /// Updates a Models.Room object within the data source.
+        /// </summary>
+        /// <param name="room"></param>
+        public void Update(Models.Room room)
+        {
+            _roomRepository.Update(room);
+        }
+
+        /// <summary>
+        /// Deletes a Models.Room object from the data source based on a Guid.
+        /// </summary>
+        /// <param name="id"></param>
+        public void Delete(Guid id)
+        {
+            _roomRepository.Delete(id);
+        }
+
+    }
+}
